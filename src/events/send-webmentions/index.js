4d64409e63958118e90793d7f8a5866f9ee57685
Jamie Tanna <jamie@jamietanna.co.uk>
2021-07-21 18:54:48 +0100

    WIP-bridgy

--- src/events/send-webmentions/index.js
+++ src/events/send-webmentions/index.js
@@ -1,7 +1,10 @@
+const arc = require('@architect/functions')
 const cheerio = require('cheerio')
 const fetch = require('node-fetch')
 const logger = require('@architect/shared/logger')
 const { isValidURL } = require('@architect/shared/utils')
+
+const SYNDICATION_LOOKUP_RETRIES = 5
 
 const denyListHosts = [
   'twitter.com',
@@ -11,6 +14,11 @@ const denyListHosts = [
   'github.com',
   'res.cloudinary.com',
   'www.swarmapp.com'
+]
+
+const syndicatedUrlsReturnedHosts = [
+  'brid.gy',
+  'news.indieweb.org',
 ]
 
 async function findLinks (url) {
@@ -32,7 +40,7 @@ async function findLinks (url) {
   return links
 }
 
-async function sendWebmention (source, target) {
+async function sendWebmention (postUrl, source, target) {
   if (!process.env.TELEGRAPH_TOKEN) return
 
   // don't try to send webmentions to hosts we know can't receive them
@@ -51,16 +59,68 @@ async function sendWebmention (source, target) {
   const message = JSON.stringify(json, null, 2)
   if (response.ok) {
     logger.info(`Queued webmention with Telegraph from ${source} to ${target}`, message)
+    updatePostWithSyndication(postUrl, target, json.location)
   } else {
     logger.error(`Error received from Telegraph for ${source} to ${target}`, message)
   }
 }
 
+async function updatePostWithSyndication(postUrl, target, statusUrl) {
+  const data = await arc.tables()
+  const post = await data.posts.get(postUrl)
+
+  // don't try to update syndication links for hosts that don't support them
+  const host = new URL(target).host
+  if(!syndicatedUrlsReturnedHosts.includes(host)) return
+
+  logger.info(`Attempting to retrieve information for ${postUrl}'s syndication to ${target} via ${statusUrl}`)
+
+  const url = retrieveStatusUrl(statusUrl)
+
+  if (url) {
+    // once it's syndicated, replace the syndication link with the syndicated link
+    const index = post.properties.syndication.indexOf(target)
+    if (index !== -1) {
+      post.properties.syndication[index] = url
+    }
+  } else {
+    logger.info('No syndicated URL could be retrieved from the Webmention result')
+  }
+}
+
+async function retrieveStatusUrl(statusUrl) {
+  for (var i = 0, len = SYNDICATION_LOOKUP_RETRIES; i < len; i++) {
+    var response = await fetch(statusUrl)
+    var json = await response.json()
+
+    // some hosts are returning the URL in the response body's `url` property
+    if (json.url && isValidURL(json.url)) {
+      return json.url
+    }
+
+    // if Telegraph doesn't provide a status URL after a poll, we shouldn't try it again
+    if (!json.location) {
+      break
+    }
+
+    await sleep(5)
+  }
+
+  return null
+}
+
+async function sleep(seconds) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, seconds * 1000);
+  });
+}
+
 exports.handler = async function subscribe (event) {
   const body = JSON.parse(event.Records[0].Sns.Message)
-  const url = new URL(body.url, process.env.ROOT_URL).href
+  const url = // new URL(body.url, process.env.ROOT_URL).href
+    'https://www.jvt.me/mf2/2021/07/zhh06/'
   const links = await findLinks(url)
   for (const link of links) {
-    await sendWebmention(url, link)
+    await sendWebmention(body.url, url, link)
   }
 }
